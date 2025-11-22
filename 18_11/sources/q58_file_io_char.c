d5421e224f7f500fe13c8c2c98a662c5a2bdfeac
Tausif Alam <devops.tausifalam@outlook.com>
2025-11-22 17:49:06 +0000

    q58

--- 18_11/sources/q58_file_io_char.c
+++ 18_11/sources/q58_file_io_char.c
@@ -6,8 +6,40 @@
 
 #include <stdio.h>
 
-int main() {
-    // Your code here
+int main()
+{
+    printf("Tausif Alam, BCA-1EA\n\n");
 
+    FILE *fp;
+
+    // Write to file
+    fp = fopen("q58.txt", "w");
+    if (fp == NULL)
+    {
+        printf("FILE NOT OPENED");
+        return 1;
+    }
+
+    printf("Enter TEXT to save: ");
+    char c[100];
+    fgets(c, 100, stdin);
+
+    fprintf(fp, "%s", c);
+    fclose(fp);
+
+    // Reopen to read
+    fp = fopen("q58.txt", "r");
+    if (fp == NULL)
+    {
+        printf("FILE NOT OPENED");
+        return 1;
+    }
+
+    char reader[100];
+    fgets(reader, 100, fp);
+
+    printf("Text read from file: %s\n", reader);
+
+    fclose(fp);
     return 0;
 }

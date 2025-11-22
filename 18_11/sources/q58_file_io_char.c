83627d20ec0c9b701be51e10d51b872e041bdd13
Tausif Alam <devops.tausifalam@outlook.com>
2025-11-22 08:25:05 +0000

    bare-bones for q58-60

---
+++ 18_11/sources/q58_file_io_char.c
@@ -0,0 +1,13 @@
+/*
+ * Program: [Program to open a file, read character and write into file, close and re-open]
+ * Author:  Tausif Alam, BCA-1EA
+ * Date:    2025-11-22
+ */
+
+#include <stdio.h>
+
+int main() {
+    // Your code here
+
+    return 0;
+}
---
+++ 18_11/sources/q60_DMA_malloc_free.c
@@ -0,0 +1,13 @@
+/*
+ * Program: [Demonstrate use of malloc and free]
+ * Author:  Tausif Alam, BCA-1EA
+ * Date:    2025-11-22
+ */
+
+#include <stdio.h>
+
+int main() {
+    // Your code here
+
+    return 0;
+}

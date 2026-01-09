8485eef00fac5d33aa8be14eadfa0cc834468e1e
Tausif Alam <devops.tausifalam@outlook.com>
2026-01-09 20:23:46 +0530

    approach 2: in p2 of project-euler

--- ProjectEuler/p2_fibonacci_sum_val_not_exceeding_4_millions_even_terms.c
+++ ProjectEuler/p2_fibonacci_sum_val_not_exceeding_4_millions_even_terms.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 // Program: To find sum of even fibonacci numbers not exceeding 4 million
+
+// Approach 1: Checking for even numbers
+// int main() {
+//     int limit = 4000000, termA = 1, termB = 1, nextTerm = termA + termB, sum = 2;
+//     while (nextTerm <= limit) {
+//         termA = termB;
+//         termB = nextTerm;
+//         nextTerm = termA + termB;
+//         if (nextTerm % 2 == 0) {
+//             sum += nextTerm;
+//         }
+//     }
+//     return sum;
+// }
+
+// Approach 2: avoiding any checks
+// Key: every third fibonacci number is even
+// termA is the former
+// termB is the latter
+// nextTerm is the sum of former + latter;
 int main() {
-    int limit = 4000000, term1 = 1, term2 = 1, nextTerm = term1 + term2, sum = 2;
+    int limit = 4000000, termA = 1, termB = 1, nextTerm = termA + termB, sum = 0;
     while (nextTerm <= limit) {
-        term1 = term2;
-        term2 = nextTerm;
-        nextTerm = term1 + term2;
-        if (nextTerm % 2 == 0) {
-            sum += nextTerm;
-        }
+        sum += nextTerm;
+        termA = termB + nextTerm;
+        termB = nextTerm + termA;
+        nextTerm = termA + termB;
     }
     return sum;
 }
